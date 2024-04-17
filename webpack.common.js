fe447be8e34f605a1558b7aa78ba1e11b29d70a3
Nayara Ribeiro <nayararibeiro@gmail.com>
2024-04-16 23:33:20 -0300

    feat: adds .env

--- webpack.common.js
+++ webpack.common.js
@@ -1,48 +1,50 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
 const rules = [
   {
     test: /\.(js|jsx|tsx|ts)$/,
     exclude: /node_modules/,
     use: {
-      loader: 'ts-loader',
+      loader: "ts-loader",
     },
   },
   {
     test: /\.css$/,
     exclude: /node_modules/,
-    use: ['style-loader', 'css-loader'],
+    use: ["style-loader", "css-loader"],
   },
   {
     test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
     exclude: /node_modules/,
     use: [
       {
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]',
-          outputPath: 'fonts/'
-        }
-      }
-    ]
-  }
+          name: "[name].[ext]",
+          outputPath: "fonts/",
+        },
+      },
+    ],
+  },
 ];
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.tsx'),
+  entry: path.join(__dirname, "src", "index.tsx"),
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
   },
   module: { rules },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
+    new Dotenv({ path: "./environment/.env", safe: true }),
   ],
 };
